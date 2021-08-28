e128a5df5646ecf024a11a4aad44d53f401aa937
kenya ohara <bureibu20@gmail.com>
2021-08-28 21:55:49 +0900

    rspec_fix

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe 'タスク管理機能', type: :system do
     @user_second = User.create(name: "test2", email: "test2@gmail.com", password: "123456", password_confirmation: "123456")
     FactoryBot.create(:task, user_id: @user.id)
     FactoryBot.create(:second_task, user_id: @user_second.id)
-    FactoryBot.create(:label)
-    FactoryBot.create(:second_label)
+    @label = FactoryBot.create(:label)
+    @label_second = FactoryBot.create(:second_label)
     visit new_session_path
     fill_in 'session[email]', with: "test@gmail.com"
     fill_in 'session[password]', with: "123456"
@@ -20,7 +20,6 @@ RSpec.describe 'タスク管理機能', type: :system do
         binding.irb
         check "test1"
         fill_in "task[title]", with: "test_title"
-        check 'task_label_ids_63' 
         fill_in "task[content]", with: "test_content"
         fill_in 'task[expired_at]', with: "002021-09-01"
         select "未着手", from: 'task[status]'
