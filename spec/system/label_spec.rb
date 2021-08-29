0920e018b0115cdad187373fbbf1bb0ec041a7a3
kenya ohara <bureibu20@gmail.com>
2021-08-29 23:05:52 +0900

    rspecテストコード追加

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -11,21 +11,39 @@ RSpec.describe 'タスク管理機能', type: :system do
     fill_in 'session[email]', with: "test@gmail.com"
     fill_in 'session[password]', with: "123456"
     click_on 'commit' 
+    visit new_task_path
+    check "task_label_ids_#{@label.id}"
+    fill_in "task[title]", with: "test_title"
+    fill_in "task[content]", with: "test_content"
+    fill_in 'task[expired_at]', with: "002021-09-01"
+    select "未着手", from: 'task[status]'
+    select "高", from: 'task[priority]'
+    select "高", from: 'task[priority]'
+    click_on "登録する"
   end
   
   describe 'ラベル機能確認' do
     context 'ラベルを新規作成した場合' do
       it '作成したラベルが表示される' do
-        visit new_task_path
-        binding.irb
-        check "test1"
-        fill_in "task[title]", with: "test_title"
-        fill_in "task[content]", with: "test_content"
-        fill_in 'task[expired_at]', with: "002021-09-01"
-        select "未着手", from: 'task[status]'
-        select "高", from: 'task[priority]'
-        select "高", from: 'task[priority]'
-        click_on "登録する"
+        expect(page).to have_content 'test1'
+      end
+    end
+  end
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it 'タスクにつけたラベルの内容が表示される' do
+        click_on 'Back'
+        all('.index_list')[1].click_link '詳細'
+        expect(page).to have_content 'test1'
+      end
+    end
+  end
+  describe '検索機能' do
+    context 'ラベル検索をした場合' do
+      it "検索したラベルがついているタスクが絞り込まれる" do
+        click_on 'Back'
+        select 'test1', from: 'label_id'
+        click_button '検索'
         expect(page).to have_content 'test1'
       end
     end
